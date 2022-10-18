83bb3e69744adb7364bb3a5ff3500d66acbf49da
JSnelgrove <jack.snelgrove@me.com>
2022-10-18 17:26:09 -0400

    Client registration activity and Cook registration activity added

--- Mealer/app/src/main/java/com/example/mealer/MainActivity.java
+++ Mealer/app/src/main/java/com/example/mealer/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.mealer;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
@@ -22,6 +23,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -35,8 +37,28 @@ public class MainActivity extends AppCompatActivity {
         Spinner userTypes= findViewById(R.id.UserTypes);
         ArrayAdapter<CharSequence>adapter=ArrayAdapter.createFromResource(this, R.array.Users, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
-
         userTypes.setAdapter(adapter);
+
+
+        Button next = findViewById(R.id.next1);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String user = userTypes.getSelectedItem().toString();
+                if (user.equals("Client")) {
+                    Intent intent = new Intent(getApplicationContext(), clientRegister.class);
+                    startActivityForResult(intent, 0);
+                }
+                else if (user.equals("Cook")){
+                    Intent intent = new Intent(getApplicationContext(),cookRegister.class);
+                    startActivityForResult(intent, 0);
+                }
+
+
+            }
+        });
+
     }
 
 }
