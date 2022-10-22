1f7630a02c85d6b48ce5d8515601de8c9ba36afc
RecklessSnips <113377805+RecklessSnips@users.noreply.github.com>
2022-10-21 21:45:17 -0400

    Update WelcomMenu.java

--- Mealer/app/src/main/java/com/example/mealer/WelcomMenu.java
+++ Mealer/app/src/main/java/com/example/mealer/WelcomMenu.java
@@ -20,33 +20,38 @@ import androidx.navigation.ui.NavigationUI;
 import com.example.mealer.databinding.ActivityWelcomMenuBinding;
 
 public class WelcomMenu extends AppCompatActivity {
-
+    private TextView message;
 
     @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_welcom_menu);
+        message = findViewById(R.id.message);
+//
+//        Bundle extras = getIntent().getExtras();
+//        int type = extras.getInt("type");
+//        TextView message=findViewById(R.id.message);
+//
+//
+//        if (type==1){
+//            message.setText("Welcome! You are logged in as Client");
+//
+//        }
+//        else if (type==0){
+//            message.setText("Welcome! You are logged in as Cook");
+//        }
+        Intent i = getIntent();
+        message.setText("Welcome "+ i.getStringExtra("firstName") + "! You have signed as a "
+                + i.getStringExtra("role") + "!");
 
-        Bundle extras = getIntent().getExtras();
-        int type = extras.getInt("type");
-        TextView message=findViewById(R.id.message);
-
-
-        if (type==1){
-            message.setText("Welcome! You are logged in as Client");
-
-        }
-        else if (type==0){
-            message.setText("Welcome! You are logged in as Cook");
-        }
         Button logoff=findViewById(R.id.logOff);
         logoff.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent=new Intent(getApplicationContext(), LogIn.class);
                 startActivity(intent);
+                finish();
             }
         });
     }
