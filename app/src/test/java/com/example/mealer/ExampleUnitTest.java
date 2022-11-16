6702185c49c472a154ab06508e536d934e979d6e
JSnelgrove <jack.snelgrove@me.com>
2022-11-16 15:09:43 -0500

    Added unit tests

--- app/src/test/java/com/example/mealer/ExampleUnitTest.java
+++ app/src/test/java/com/example/mealer/ExampleUnitTest.java
@@ -5,14 +5,38 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import com.example.mealer.Accounts.Client;
+import com.example.mealer.Accounts.Cook;
+import com.example.mealer.Accounts.Meal;
+
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ * @see <a href="http://d.android.com/tools/testing%22%3ETesting documentation</a>
  */
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void checkCookStatus_0() {
+        Cook cook = new Cook("Anakin", "SkyWalker", "Sand", "501", "Courscant", "Cheque123", "The chosen one");
+        cook.setIsActive("Suspend temporarily");
+        assertEquals("Check if the Cook status is banned", "Suspend temporarily", cook.getIsActive());
+    }
+
+    @Test
+    public void checkCookStatus_1() {
+        Cook cook = new Cook();
+        assertEquals("Check if the new Cook status is active", "active", cook.getIsActive());
+    }
+
+    @Test
+    public void checkClientName() {
+        Client client = new Client("Shuai", "Gao", "cgao034@uottawa.ca", "123", "London686", "1314520");
+        assertEquals("Check if the client's address is correct", "London686", client.getAddress());
+    }
+
+    @Test
+    public void checkMealCookID() {
+        Meal meal = new Meal("Burger", "Cook1");
+        assertEquals("Check if the meal matches the cook id", "Cook1", meal.getCookID());
     }
 }
